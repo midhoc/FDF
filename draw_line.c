23c0d2d33742179d489969410e54e82e5e82691c
Hocine MIDOUN <hmidoun@e1r12p23.42.fr>
2019-08-05 03:25:37 +0200

    iso is working

--- draw_line.c
+++ draw_line.c
@@ -6,133 +6,84 @@
 /*   By: hmidoun <hmidoun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 00:04:40 by hmidoun           #+#    #+#             */
-/*   Updated: 2019/08/05 02:50:57 by hmidoun          ###   ########.fr       */
+/*   Updated: 2019/08/05 03:24:43 by hmidoun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	draw_line_x1_dx(t_p_list p1, t_p_list p2, void *mlx_ptr, void *win_ptr)
+void	draw_line_x(t_p_list p1, t_p_list p2, void *mlx_ptr, void *win_ptr)
 {
-	int dp;
-	int deltaE;
-	int deltaNE;
+	int dx;
+	int dy;
+	int yi;
+	int d;
 
-	deltaE = 2 * fmin(p1.y, p2.y);
-	deltaNE = 2 * (fmin(p1.y, p2.y) - fmin(p1.x, p2.x));
-	dp = 2 * fmin(p1.y, p2.y) - fmin(p1.x, p2.x);
-	mlx_pixel_put(mlx_ptr, win_ptr, p1.x, p1.y, 100000);
+	dx = p2.x - p1.x;
+	dy = p2.y - p1.y;
+	yi = 1;
+	if (dy < 0)
+	{
+		yi = -1;
+		dy = -dy;
+	}
+	d = 2 * dy - dx;
 	while (p1.x < p2.x)
 	{
-		if (dp <= 0)
-		{
-			dp = dp + deltaE;
-			p1.x++;
-		}
-		else
-		{
-			dp = dp + deltaNE;
-			p1.x++;
-			p1.y++;
-		}
 		mlx_pixel_put(mlx_ptr, win_ptr, p1.x, p1.y, 100000);
+		if (d > 0)
+		{
+			p1.y += yi;
+			d = d - 2 * dx;
+		}
+		d = d + 2 * dy;
+		p1.x++;
 	}
 }
-void	draw_line_x1_dy(t_p_list p1, t_p_list p2, void *mlx_ptr, void *win_ptr)
-{
-	int dp;
-	int deltaE;
-	int deltaNE;
 
-	deltaE = 2 * fmin(p1.x, p2.x);
-	deltaNE = -2 * (fmin(p1.y, p2.y) - fmin(p1.x, p2.x));
-	dp = 2 * fmin(p1.x, p2.x) - fmin(p1.y, p2.y);
-	mlx_pixel_put(mlx_ptr, win_ptr, p1.x, p1.y, 100000);
+void	draw_line_y(t_p_list p1, t_p_list p2, void *mlx_ptr, void *win_ptr)
+{
+	int dx;
+	int dy;
+	int xi;
+	int d;
+
+	dx = p2.x - p1.x;
+	dy = p2.y - p1.y;
+	xi = 1;
+	if (dx < 0)
+	{
+		xi = -1;
+		dx *= -1;
+	}
+	d = 2 * dx - dy;
 	while (p1.y < p2.y)
 	{
-		if (dp <= 0)
-		{
-			dp = dp + deltaE;
-			p1.y++;
-		}
-		else
-		{
-			dp = dp + deltaNE;
-			p1.x++;
-			p1.y++;
-		}
 		mlx_pixel_put(mlx_ptr, win_ptr, p1.x, p1.y, 100000);
-	}
-}
-
-void	draw_line_x2_dy(t_p_list p1, t_p_list p2, void *mlx_ptr, void *win_ptr)
-{
-	int dp;
-	int deltaE;
-	int deltaNE;
-
-	deltaE = 2 * fmin(p1.x, p2.x);
-	deltaNE = -2 * (fmin(p1.y, p2.y) - fmin(p1.x, p2.x));
-	dp = 2 * fmin(p1.x, p2.x) - fmin(p1.y, p2.y);
-	mlx_pixel_put(mlx_ptr, win_ptr, p1.x, p1.y, 100000);
-	while (p1.y < p2.y)
-	{
-		if (dp <= 0)
+		if (d > 0)
 		{
-			dp = dp + deltaE;
-			p1.y++;
+			p1.x += xi;
+			d = d - 2 * dy;
 		}
-		else
-		{
-			dp = dp + deltaNE;
-			p1.x--;
-			p1.y++;
-		}
-		mlx_pixel_put(mlx_ptr, win_ptr, p1.x, p1.y, 100000);
-	}
-}
-
-void	draw_line_x2_dx(t_p_list p1, t_p_list p2, void *mlx_ptr, void *win_ptr)
-{
-	int dp;
-	int deltaE;
-	int deltaNE;
-
-	deltaE = 2 * fmin(p1.y, p2.y);
-	deltaNE = 2 * (fmin(p1.y, p2.y) - fmin(p1.x, p2.x));
-	dp = 2 * fmin(p1.y, p2.y) - fmin(p1.x, p2.x);
-	mlx_pixel_put(mlx_ptr, win_ptr, p1.x, p1.y, 100000);
-	while (p1.x < p2.x)
-	{
-		if (dp <= 0)
-		{
-			dp = dp + deltaE;
-			p1.x--;
-		}
-		else
-		{
-			dp = dp + deltaNE;
-			p1.x--;
-			p1.y++;
-		}
-		mlx_pixel_put(mlx_ptr, win_ptr, p1.x, p1.y, 100000);
+		d = d + 2 * dx;
+		p1.y++;
 	}
 }
 
 void	draw_line(t_p_list p1, t_p_list p2, void *mlx_ptr, void *win_ptr)
 {
-	if (p2.x > p1.x)
+	if (abs(p1.y - p2.y) < abs(p1.x - p2.x))
 	{
-		if (fmin(p1.y, p2.y) <= fmin(p1.x, p2.x))
-			draw_line_x1_dx(p1, p2, mlx_ptr, win_ptr);
+		if (p1.x > p2.x)
+			draw_line_x(p2, p1, mlx_ptr, win_ptr);
 		else
-			draw_line_x1_dy(p1, p2, mlx_ptr, win_ptr);
+			draw_line_x(p1, p2, mlx_ptr, win_ptr);
 	}
 	else
 	{
-		if (fmin(p1.y, p2.y) <= fmin(p1.x, p2.x))
-			draw_line_x2_dx(p1, p2, mlx_ptr, win_ptr);
+		if (p1.y > p2.y)
+			draw_line_y(p2, p1, mlx_ptr, win_ptr);
 		else
-			draw_line_x2_dy(p1, p2, mlx_ptr, win_ptr);
+			draw_line_y(p1, p2, mlx_ptr, win_ptr);
 	}
 }
