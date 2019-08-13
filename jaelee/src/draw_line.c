28d1855f164af14e719e896a28fcf65c5705390c
Hocine MIDOUN <hmidoun@e1r1p14.42.fr>
2019-08-13 02:38:04 +0200

    added color change variation

--- jaelee/src/draw_line.c
+++ jaelee/src/draw_line.c
@@ -6,7 +6,7 @@
 /*   By: hmidoun <hmidoun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 00:04:40 by hmidoun           #+#    #+#             */
-/*   Updated: 2019/08/12 08:53:05 by hmidoun          ###   ########.fr       */
+/*   Updated: 2019/08/13 02:36:59 by hmidoun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,6 +96,41 @@ double percent(int start, int end, int current)
 	return ((distance == 0) ? 1.0 : (placement / distance));
 }
 
+static	double percent_atan(int start, int end, int current)
+{
+	double placement;
+	double distance;
+	double percent;
+	int		trans;
+	float	offset;
+
+
+	placement = current - start;
+	distance = end - start;
+
+
+offset = 1; //0  < >2  change it to chage the color variation  0 to see the bottom of the map .... 2 the top
+	trans = start + (distance * 0.5) * offset;
+
+
+	percent = atan(((placement + trans) * 20 / distance )- 10)/(M_PI_2) + 1;
+	//printf("%f\t%f\t%f\n", atan(((placement) * 20 / distance )- 10), percent, placement);
+	return ((distance == 0) ? 1.0 : percent);
+}
+
+// static	double percent_x3(int start, int end, int current)
+// {
+// 	double placement;
+// 	double distance;
+// 	double percent;
+
+// 	placement = current - start;
+// 	distance = end - start;
+// 	percent = powf(((placement) * 6 / distance )- 3,3)/(27);
+// 	//printf("%f\t%f\t%f\n", atan(((placement) * 20 / distance )- 10), percent, placement);
+// 	return ((distance == 0) ? 1.0 : percent);
+// }
+
 int get_light(int start, int end, double percentage)
 {
 	return ((int)((1 - percentage) * start + percentage * end));
@@ -113,16 +148,16 @@ int 	get_color(int current, t_info start, t_info end, int i)
 	else if(i == 0)
 		percentage = percent(start.y, end.y, current);
 	else
-		percentage = percent(start.z, end.z, current);
+		percentage = percent_atan(start.z, end.z, current);
 	red = get_light((start.color >> 16) & 0xFF,
 			(end.color >> 16) & 0xFF, percentage);
 	green = get_light((start.color >> 8) & 0xFF,
 			(end.color >> 8) & 0xFF, percentage);
 	blue = get_light(start.color & 0xFF, end.color & 0xFF, percentage);
-	//printf("%d\n", (red << 16) | (green << 8) | blue); something wrong to fix here
 	return ((red << 16) | (green << 8) | blue);
+	//return (get_light(start.color, end.color, percentage));
 }
-/*
+
 static	void	z_max_min(t_fdf_info *fdf)
 {
 	int	i;
@@ -138,18 +173,20 @@ static	void	z_max_min(t_fdf_info *fdf)
 			fdf->z_min = ((t_info*)fdf->grid.ptr)[i].z;
 	}
 }
-*/
+
 void	set_color(t_array grid, t_fdf_info *fdf)
 {
 	size_t	i;
 	t_info	t1;
 	t_info	t2;
 
-	//z_max_min(fdf);
-	t1.z = -10;
-	t2.z = 20;
-	t1.color = GREEN;
-	t2.color = RED;
+	z_max_min(fdf);
+	t1.z = fdf->z_min;
+	t2.z = fdf->z_max;
+	// t1.z = -1000;
+	// t2.z = 1000;
+	t1.color = 0x00ff00;
+	t2.color = 0xff0000;
 	i = -1;
 	while (++i < grid.length)
 	{
